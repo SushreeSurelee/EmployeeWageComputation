93e200b9250fba59af9950568620a0d8556fe415
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-22 21:23:50 +0530

    [Sushree | Added fuctionality for part time employee]

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -12,16 +12,20 @@ namespace EmployeeWageComputation
         { 
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             Random random = new Random();
-
             int empHrs = 0;
             int empWage = 0;
 
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
