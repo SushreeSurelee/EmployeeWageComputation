c13724acb6d56a6dcc73dace7c9f6099b99e81dc
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-22 21:16:28 +0530

    [Sushree | Added fuctionality to calculate daily employee wage]

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -13,19 +13,24 @@ namespace EmployeeWageComputation
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
 
             Random random = new Random();
+
+            int empHrs = 0;
+            int empWage = 0;
+
             int empCheck = random.Next(0, 2);
-            Console.WriteLine(empCheck);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present.");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent.");
+                empHrs = 0;
             }
-
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Daily employee wage = " + empWage);
             Console.ReadLine();
         }
     }
