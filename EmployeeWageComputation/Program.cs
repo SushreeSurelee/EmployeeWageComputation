f2521e1aa32c18aab577446c00161a300d98a159
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-22 22:06:07 +0530

    [Sushree | Added functionality to calculate wages for a month]

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,34 +8,36 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
-        { 
+        {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            const int IS_PART_TIME = 1;
-            const int IS_FULL_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
             Random random = new Random();
-            int empHrs = 0;
-            int empWage = 0;
-
-            int empCheck = random.Next(0, 3);
-
-            switch (empCheck)
+            for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Daily Emp Wage = " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Daily employee wage = " + empWage);
+            Console.WriteLine("Monthly employee wage = " + totalEmpWage);
             Console.ReadLine();   
         }
     }
