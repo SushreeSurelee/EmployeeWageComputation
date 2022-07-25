434fab1543e694992998414e0e15da82ae66126a
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-25 08:41:17 +0530

    [Sushree | Added functitonality to save total wage for each company]

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,16 +6,31 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    class Program
+
+    class EmpWageObjects
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
 
-        public static int CalculateEmpWage(string company, int numOfWorkingDays, int empRatePerHr, int maxHoursPerMonth)
+        private string company;
+        private int numOfWorkingDays;
+        private int empRatePerHr;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public EmpWageObjects(string company, int numOfWorkingDays, int empRatePerHr, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.empRatePerHr = empRatePerHr;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void CalculateEmpWage()
         {
             int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
             Random random = new Random();
-            while (totalWorkingDays < numOfWorkingDays && totalEmpHrs <= maxHoursPerMonth)
+            while (totalWorkingDays < this.numOfWorkingDays && totalEmpHrs <= this.maxHoursPerMonth)
             {
                 totalWorkingDays++;
                 int empCheck = random.Next(0, 3);
@@ -34,16 +49,29 @@ namespace EmployeeWageComputation
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day number = " + totalWorkingDays + " Working hours = " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * empRatePerHr;
-            Console.WriteLine("Total Employee Wage for " +company+ " is " +totalEmpWage);
-            return totalEmpWage;
+            totalEmpWage = totalEmpHrs * this.empRatePerHr;
+            Console.WriteLine("Total Employee Wage for " + company + " is " + totalEmpWage);
         }
+
+        public string ToString()
+        {
+            return ("Total wage for " + this.company + " is " + this.totalEmpWage);
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program\n");
-            CalculateEmpWage("TCS",25,18,80);
-            CalculateEmpWage("Wipro", 20, 25, 90);
-            Console.ReadLine();   
+            EmpWageObjects TCS = new EmpWageObjects("TCS", 25, 18, 80);
+            TCS.CalculateEmpWage();
+            Console.WriteLine(TCS.ToString());
+
+            EmpWageObjects Wipro = new EmpWageObjects("Wipro", 20, 25, 90);
+            Wipro.CalculateEmpWage();
+            Console.WriteLine(Wipro.ToString());
+
+            Console.ReadLine();
         }
     }
 }
